da391c33202bf38e6f28b44a13d55e6c1244266a
ViniciusCruzMoura <contato.cruz.moura@gmail.com>
2024-08-31 20:06:57 -0400

    change(questao_3): calc venda camiseta no atacado

--- logica_programacao_algoritmos/questao_3.py
+++ logica_programacao_algoritmos/questao_3.py
@@ -1,38 +1,137 @@
+from decimal import Decimal
+
+def input_numerico(texto):
+    val = None
+    while True:
+        try:
+            val = input(texto)
+            val = str(val).replace(",", ".")
+            val = int(Decimal(val))
+        except Exception:
+            print("Valor invalido, porfavor digite um valor numerico valido!")
+            continue
+        break
+    return val
+
 def escolha_modelo():
-    #Pergunta o modelo desejado
-    #Retorna o valor do modelo com base na escolha do usuário
-    #Repete a pergunta do item. se digitar uma opção diferente de: MCS/MLS/MCE/MLE
-    return input("Entre com o modelo desejado")
+    modelo = None
+
+    # Pergunta o modelo desejado
+    # Repete a pergunta se digitar uma opção diferente de: MCS/MLS/MCE/MLE
+    while not modelo:
+        modelo = input("""
+Entre com o Modelo Desejado:
+MCS - Manga Curta simples
+MLS - Manga Longa simples
+MCE - Manga Curta com estampa
+MLE - Manga Longa com estampa
+>>""").upper()
+        if modelo != "MCS" \
+        and modelo != "MLS" \
+        and modelo != "MCE" \
+        and modelo != "MLE" :
+            print("Escolha invalida!, entre com o modelo novamente")
+            modelo = None
+
+    # Retorna o valor do modelo com base na escolha do usuário
+    if modelo == "MCS":
+        return 180
+    elif modelo == "MLS":
+        return 210
+    elif modelo == "MCE":
+        return 290
+    elif modelo == "MLE":
+        return 320
+    else:
+        return None
+    return None
 
 def num_camisetas():
-    #Pergunta o número de camisetas;
-    #Retorna (use return) o número de camisetas com desconto seguindo a regra do enunciado (desconto calculado em cima do número de camisetas
-    #Repete a pergunta do item. se digitar um valor acima de 20000 ou valor não numérico (use try/except para não numérico
-    return input("Entre com a quantidade de camisetas")
+    quantidade_de_camiseta = None
+
+    # Pergunta o número de camisetas;
+    # Repete a pergunta se digitar um valor acima de 20000 ou valor não numérico
+    # usar try catch
+    while not quantidade_de_camiseta:
+        quantidade_de_camiseta = input("Entre com a quantidade de camisetas\n>>")
+        # Validar valor não numerico
+        try:
+            quantidade_de_camiseta = int(quantidade_de_camiseta)
+        except Exception:
+            print("Valor Invalido!")
+            quantidade_de_camiseta = None
+            continue
+        # Limite de quantidade
+        if quantidade_de_camiseta > 20000:
+            print("Não é aceito pedidos nessa quantidade de camisetas!")
+            quantidade_de_camiseta = None
+            continue
+
+    #Retorna o número de camisetas com desconto
+    if quantidade_de_camiseta < 20:
+        return int(( (100 - 0) * quantidade_de_camiseta ) / 100)
+    elif quantidade_de_camiseta >= 20 and quantidade_de_camiseta < 200:
+        return int(( (100 - 5) * quantidade_de_camiseta ) / 100)
+    elif quantidade_de_camiseta >= 200 and quantidade_de_camiseta < 2000:
+        return int(( (100 - 7) * quantidade_de_camiseta ) / 100)
+    elif quantidade_de_camiseta >= 2000 and quantidade_de_camiseta <= 20000:
+        return int(( (100 - 12) * quantidade_de_camiseta ) / 100)
+
+    return None
 
 def frete():
+    tipo_de_frete = None
+
     #Pergunta pelo serviço adicional de frete
-    #Retorna (use return) o valor de apenas uma das opções de frete
     #Repetir a pergunta item. se digitar uma opção diferente de: 1/2/0;
-    return input("Escolha o tipo de frete")
+    while not tipo_de_frete:
+        tipo_de_frete = input("""
+Escolha o tipo de frete:
+1 - Frete por transportadora - R$ 100.00
+2 - Frete por sedex - R$ 200.00
+0 - Retirar na fabrica - R$ 0.00
+>>""")
+        # Validar valor não numerico
+        try:
+            tipo_de_frete = int(tipo_de_frete)
+        except Exception:
+            print("Valor Invalido!")
+            tipo_de_frete = None
+            continue
+        if tipo_de_frete != 1 \
+        and tipo_de_frete != 2 \
+        and tipo_de_frete != 0:
+            print("Escolha invalida!, entre com o tipo de frete novamente")
+            tipo_de_frete = None
+
+    #Retorna o valor das opções de frete
+    if tipo_de_frete == 1:
+        return 10000
+    elif tipo_de_frete == 2:
+        return 20000
+    elif tipo_de_frete == 0:
+        return 0
+    return None
 
 def main():
     # Cobrança de serviços de uma fábrica que vende Camisetas em atacado.
 
     # Dados
-    modelo_de_camiseta = None
+    valor_do_modelo_da_camiseta = None
     quantidade_de_camiseta = None
-    tipo_de_frete = None
+    valor_do_tipo_de_frete = None
     total_a_pagar = None
 
     print("Bem Vindo ao Estabelecimento do Vinicius Eduardo Carvalho da Cruz de Moura")
     
-    modelo_de_camiseta = escolha_modelo()
+    # Rotinas
+    valor_do_modelo_da_camiseta = escolha_modelo()
     quantidade_de_camiseta = num_camisetas()
-    tipo_de_frete = frete()
+    valor_do_tipo_de_frete = frete()
+    total_a_pagar = valor_do_modelo_da_camiseta * quantidade_de_camiseta + valor_do_tipo_de_frete
 
     # Saida dos Valores 
-    print("Total", "R$", "16040.00", "(Modelo : 1.80 * Quantidade(com desconto): 8800 + frete: 200.00 )")
+    print("Total", "R$", "{0:.2f}".format(total_a_pagar/100), "(Modelo :", "{0:.2f}".format(valor_do_modelo_da_camiseta/100), "* Quantidade(com desconto): ", quantidade_de_camiseta ,"+ frete:", "{0:.2f}".format(valor_do_tipo_de_frete/100), ")")
     
 if __name__ == '__main__':
     main()
